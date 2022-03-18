0cc93b91d95ba2502fceb6eccf7aece15f36d052
JochenVanCamp <vancampjochen@gmail.com>
2022-03-18 08:16:26 +0100

    Vertalingen toegevoegd

---
+++ src/persistence/language.java
@@ -0,0 +1,27 @@
+package persistence;
+import java.util.*;
+
+public class language {
+    static public ResourceBundle rb;
+    Scanner myScanner = new Scanner(System.in);
+    String gekozenTaal;
+    String[] talen = {"en", "nl", "fr"};
+
+    public void keuze(){
+        do{
+            System.out.print("Please pick your preferred language! (fr, nl, en): ");
+            gekozenTaal = myScanner.next();
+        }while(!Arrays.toString(talen).contains(gekozenTaal.toLowerCase()) || gekozenTaal.length() != 2);
+    }
+
+
+
+    public ResourceBundle taal(){
+        if (rb == null)
+        {
+            Locale.setDefault(new Locale(gekozenTaal));
+            rb = ResourceBundle.getBundle("java/cfg/language");
+        }
+        return rb;
+    }
+}
