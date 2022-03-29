2e0203e91d769d393fa94d6ad679628d250e5f8d
Naoufalbzz <naoufal.bouazzaoui@student.hogent.be>
2022-03-29 14:49:40 +0200

    Test aangepast

--- src/testen/TestSpeler.java
+++ src/testen/TestSpeler.java
@@ -1,6 +1,7 @@
 package testen;
 
 import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
